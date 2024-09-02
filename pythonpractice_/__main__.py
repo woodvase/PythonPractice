401641f12b24634b3a825f3e1241e6cfa6c1fccd
woodvase <yongcai.liu@outlook.com>
2024-09-02 16:38:18 -0700

    First Push

---
+++ pythonpractice_/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for pythonpractice_."""
+
+from pythonpractice_.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ tests/test_base.py
@@ -0,0 +1,5 @@
+from pythonpractice_.base import NAME
+
+
+def test_base():
+    assert NAME == "pythonpractice_"
