1397805b28944e441587bdf700f3603982de9ea1
woodvase <yongcai.liu@outlook.com>
2024-09-16 21:56:58 -0700

    Q 1047

---
+++ pythonpractice/1047-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stk = []
+        for c in s:
+            if(len(stk) == 0 or c != stk[len(stk)-1]):
+                stk.append(c)
+            else:
+                if(c == stk[len(stk)-1]):
+                    stk.pop()
+        return ''.join(stk)
+    
+
+print(Solution().removeDuplicates("azxxzy"))
