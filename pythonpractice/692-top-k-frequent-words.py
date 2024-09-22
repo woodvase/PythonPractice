7f7bcc1045674b53b6f2f084e716c8b8c4f0910e
woodvase <yongcai.liu@outlook.com>
2024-09-22 12:23:24 -0700

    q 692

---
+++ pythonpractice/692-top-k-frequent-words.py
@@ -0,0 +1,10 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        from collections import Counter
+        s = Counter(words)
+        sortByFrequency = sorted(s, key=lambda x: (-s[x], x))
+        return sortByFrequency[:k]
