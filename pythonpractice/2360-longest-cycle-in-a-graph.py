14fbeb8ffbd6218728ddcd4648bab7c28a084633
woodvase <yongcai.liu@outlook.com>
2024-12-18 10:56:03 -0800

    improvment for 2360

--- pythonpractice/2360-longest-cycle-in-a-graph.py
+++ pythonpractice/2360-longest-cycle-in-a-graph.py
@@ -6,13 +6,13 @@ class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         n = len(edges)
 
-        inDegrees = defaultdict(set)
-        for i, node in enumerate(edges):
-            if node > -1:
-                inDegrees[node].add(i)
+        inDegrees = [0] * n
+        for e in edges:
+            if e != -1:
+                inDegrees[e] += 1
         q = deque()
         for i in range(n):
-            if len(inDegrees[i]) == 0:
+            if inDegrees[i] == 0:
                 q.append(i)
         out = set()
         while len(q):
@@ -20,30 +20,33 @@ class Solution:
             out.add(i)
             neighbor = edges[i]
             if neighbor != -1:
-                v = inDegrees[neighbor]
-                v.remove(i)
-                if len(v) == 0:
+                inDegrees[neighbor] -= 1
+                if inDegrees[neighbor] == 0:
                     q.append(neighbor)
+
         if len(out) == n:
             return -1
 
-        incycles = set(range(n)) - out
+        incycles = [x for x in range(n) if x not in out]
         cycleLen = defaultdict(int)
         visisted = set()
 
-        def dfs(n, inDegrees, li):
+        def dfs(n, li):
             if n in visisted:
                 return
             visisted.add(n)
             cycleLen[li] += 1
-            for x in inDegrees[n]:
-                dfs(x, inDegrees, li)
+            dfs(edges[n], li)
 
         for i in incycles:
             if i not in visisted:
-                dfs(i, inDegrees, i)
+                dfs(i, i)
         max = 0
         for c in cycleLen.values():
             if c > max:
                 max = c
         return max
+
+s = Solution()
+print(s.longestCycle([3, 3, 4, 2, 3]))
+print(s.longestCycle([2, -1, 3, 1]))
